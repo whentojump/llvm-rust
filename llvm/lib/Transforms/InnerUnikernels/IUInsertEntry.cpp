2296bba0a834b00172b39e99c20832b12ab952f3
Jinghao Jia <jinghao7@illinois.edu>
2022-10-06 16:23:45 -0500

    init pass template
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

---
+++ llvm/lib/Transforms/InnerUnikernels/IUInsertEntry.cpp
@@ -0,0 +1,42 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/raw_ostream.h"
+#include <string>
+
+using namespace llvm;
+
+#define DEBUG_TYPE "iu-entry-insertion"
+
+STATISTIC(NumInserted,  "Number of entry function inserted");
+
+namespace { // Begin Anonymous namespace
+
+  class IUEntryInsertion : public ModulePass {
+  public:
+    static char ID;
+    static StringMap<std::string> MangledNameMap;
+
+    IUEntryInsertion() : ModulePass(ID) {}
+    bool runOnModule(Module &) override;
+    void getAnalysisUsage(AnalysisUsage &) const override;
+  };
+
+} // End Anonymous namespace
+
+char IUEntryInsertion::ID = 0;
+
+// Registration
+static RegisterPass<IUEntryInsertion> X("iu-entry-insertion",
+        "Insert inner-unikernel entry function",
+        false,
+        false);
+
+void IUEntryInsertion::getAnalysisUsage(AnalysisUsage &AU) {
+  // Does it alter CFG?
+  // The pass will certainly add more functions to the Module...
+}
+
+bool IUEntryInsertion::runOnModule(Module &M) {
+  return false;
+}
